76a7efb49adf8f0dde664354908073035949dda4
Jose Eduardo <130613203+Ed0se@users.noreply.github.com>
2026-05-13 11:16:14 -0700

    Delete Desktop/Semestre 2026 I/WILE/hells_angels directory
    Corregir error para hacer deploy en pages

--- Desktop/Semestre 2026 I/WILE/hells_angels/club/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class ClubConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'club'
-    verbose_name = "Club Hell's Angels"
